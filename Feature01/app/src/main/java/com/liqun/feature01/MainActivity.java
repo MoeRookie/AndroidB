34114252c4d9aa1e957adb1aa07bc29a58f80c55
MoeRookie <MoeRookie@163.com>
2020-07-22 11:44:59 +0800

    1.1.1.典型情况下的生命周期分析

--- Feature01/app/src/main/java/com/liqun/feature01/MainActivity.java
+++ Feature01/app/src/main/java/com/liqun/feature01/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 // 1.1. Activity的生命周期全面分析
+// 1.1.1. 典型情况下的生命周期分析
 public class MainActivity extends AppCompatActivity {
 
     @Override
