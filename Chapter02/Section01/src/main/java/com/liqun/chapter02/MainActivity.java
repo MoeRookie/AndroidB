63ebe4782d0fb6e01e79f043504cfc3653607bd5
MoeRookie <MoeRookie@163.com>
2020-07-24 17:32:58 +0800

    2.1.AndroidIPC简介

--- Chapter02/Section01/src/main/java/com/liqun/chapter02/MainActivity.java
+++ Chapter02/Section01/src/main/java/com/liqun/chapter02/MainActivity.java
@@ -3,7 +3,7 @@ package com.liqun.chapter02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+// 2.1. AndroidIPC简介
 public class MainActivity extends AppCompatActivity {
 
     @Override
