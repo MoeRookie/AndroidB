f2e3d6393717edd52f34205b34f98dd33def038b
MoeRookie <MoeRookie@163.com>
2020-07-26 06:03:58 +0800

    2.2.1.开启多进程模式

--- Chapter02/Section01/src/main/java/com/liqun/chapter02/MainActivity.java
+++ Chapter02/Section01/src/main/java/com/liqun/chapter02/MainActivity.java
@@ -2,14 +2,26 @@ package com.liqun.chapter02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 // 2.1. AndroidIPC简介
 // 2.2. Android中的多进程模式
+// 2.2.1. 开启多进程模式
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_enter_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ Chapter02/Section01/src/main/java/com/liqun/chapter02/ThirdActivity.java
@@ -0,0 +1,6 @@
+package com.liqun.chapter02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+}
